d919531800ae95abc1d6e8f395c1ab7391dda774
Ravinder Chadha <ravinderchadha1233@gmail.com>
2021-08-19 22:15:29 +0530

    solved more questions

---
+++ Array/q0.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    reverse(v.begin(), v.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
--- Array/q1.cpp
+++ Array/q1.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int max=0,a; //here according to question we must use array
-    for(int i=0;i<n;i++){
-        cin>>a; //but I have written an easy code for the same purpose
-        if(max<=a)
-        max=a;
+    cin >> n;
+    int max = 0, a; //here according to question we must use array
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a; //but I have written an easy code for the same purpose
+        if (max <= a)
+            max = a;
     }
-    cout<<max<<endl;
+    cout << max << endl;
 }
